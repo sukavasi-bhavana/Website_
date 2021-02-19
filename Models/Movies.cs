2e8e6e03fa55bcd30a29289d9e6ac0dfbff3e4e6
sukavasi-bhavana <sukavasibhavana@gmail.com>
2021-02-19 11:17:41 -0800

    Update Movies.cs

--- Models/Movies.cs
+++ Models/Movies.cs
@@ -7,6 +7,7 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        
         public int ID { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
